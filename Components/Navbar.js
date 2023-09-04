5db0551b09511b4f27072c00e1b4ebbaae209d58
ryeCoding06 <84782494+Rye-06@users.noreply.github.com>
2023-09-04 19:17:21 -0400

    added routing
    . Added routing for different pages
    . Made navbar a component

---
+++ Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav class="navbar">
+    <a href="/" class="nav-item">Home</a>
+    <a href="/About" class="nav-item">About</a>
+    <a href="/Contact" class="nav-item">Contact</a>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
--- app/page.js
+++ app/page.js
@@ -1,19 +1,16 @@
 "use client"
 import React from 'react'
+import Navbar from '@/Components/Navbar'
 
 const page = () => {
   return (
     <>
-    <nav class="navbar">
-      <a href="#" class="nav-item">Home</a>
-      <a href="#" class="nav-item">About</a>
-      <a href="#" class="nav-item">Contact</a>
-    </nav>
+    <Navbar />
     <h1>Talk and Walk</h1>
     <h2>It's time to explore what the world can bring you.</h2>
     <div className='wrapper'>
-      <a class="btn" href="" target="_blank"><span class="btn-text">Register</span></a>
-      <a class="btn" href="" target="_blank"><span class="btn-text">Login</span></a>
+      <a class="btn" href='/Register'><span class="btn-text">Register</span></a>
+      <a class="btn" href='/Login'><span class="btn-text">Login</span></a>
     </div>
     </>
   )
