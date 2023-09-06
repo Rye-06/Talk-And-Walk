bdcdeb1a10c2b7c861ac1683727d898d7f2452ed
ryeCoding06 <84782494+Rye-06@users.noreply.github.com>
2023-09-06 19:22:12 -0400

    added logout
    . User is able to log out of the account
    . All cookies are deleted

--- Components/loginNavbar/loginNavbar.js
+++ Components/loginNavbar/loginNavbar.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './loginNavbar.css'
+import Cookies from 'universal-cookie';
 
 const LoginNavbar = () => {
 
+  const cookie = new Cookies();
+
   const logout = () => {
-    
+    // delete all cookies
+    cookie.remove('name')
+    cookie.remove('email')
+    cookie.remove('password')
   }
 
   return (
@@ -13,7 +19,7 @@ const LoginNavbar = () => {
     <a href="/Home" className="nav-item">Home</a>
     <a href="/About" className="nav-item">About</a>
     <a href="/Contact" className="nav-item">Contact</a>
-    <a href="/" className="nav-item">Logout</a>
+    <a href="" onClick={logout} className="nav-item">Logout</a>
     </nav>
     </>
   )
